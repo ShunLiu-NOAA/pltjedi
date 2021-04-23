f0635748c40a4ce19834653e1218b0a4b15895c3
Shun Liu <sliu@Orion-login-4.HPC.MsState.Edu>
2021-04-23 11:30:31 -0500

    plt.sh

---
+++ plt.sh
@@ -0,0 +1,2 @@
+python plt_ufo_omb.py -i /work/noaa/da/sliu/R2D2/gfs/diag/a05q/PT6H/2020-12-16/gfs.a05q.diag.PT6H.sondes.2020-12-16T21:00:00Z.PT6H.nc4  \
+                    -otype T -vname air_temperature 
