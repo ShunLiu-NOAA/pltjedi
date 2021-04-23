5cce92ffc4d06c3ca0a34d884e92dce5a2c51f61
Shun Liu <sliu@Orion-login-4.HPC.MsState.Edu>
2021-04-23 11:30:25 -0500

    plt_ufo_omb.py

--- plt_ufo_omb.py
+++ plt_ufo_omb.py
@@ -4,93 +4,63 @@ import statistics
 import math
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
+import argparse
 
 #from datetime import datetime
 #import matplotlib.colors as colors
 #import cartopy.crs as ccrs
 #import cartopy.feature as cfeature
 
-def plt_ref_vertical(filename):
+def plt_ufo_t(filename,OBSTYPE,VarName):
 
+   thisobstype=OBSTYPE
+   thisvarname=VarName
+   gsihofXBc=thisvarname+'@GsiHofXBc'
+   gsihofX  =thisvarname+'@GsiHofX'
+   ufohofX  =thisvarname+'@hofx'
    f=Dataset(filename, mode='r')
-   gsi_observer_withqc=f.variables['radial_velocity@GsiHofXBc'][:]
-   gsi_observer_noqc=f.variables['radial_velocity@GsiHofX'][:]
-   geopotential_height=f.variables['geopotential_height@MetaData'][:]
-   ufo=f.variables['radial_velocity@hofx'][:]
+   gsi_observer_hofXBc=f.variables[gsihofXBc][:]
+   gsi_observer_hofX  =f.variables[gsihofX][:]
+   ufo                =f.variables[ufohofX][:]
+   geopotential_height=f.variables['height@MetaData'][:]
+#  gsi_observer_withqc=f.variables['radial_velocity@GsiHofXBc'][:]
    f.close()
 
-   fig = plt.figure(figsize=(8.0,7.5))
-   ax=fig.add_subplot(111)
 
-   diff=gsi_observer_noqc
-   diff=diff - ufo
-   print(diff)
-
-   rms=float(0)
-   for x in diff:
-      rms=rms+x*x
-   rms=math.sqrt(rms/len(diff))
-   print(rms)
-#  print(statistics.stdev(diff))
-
-   #plt.plot(gsi_observer,ufo, color='black',label="obs" , linewidth=2, marker='o')
-#  plt.scatter(gsi_observer_withqc,ufo, color='blue',label="rw", marker='o', s=3)
-   plt.scatter(gsi_observer_noqc,ufo, color='r',label="rw", marker='o', s=3)
-
-   #box = ax.get_position()
-   #ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-
-# Put a legend to the right of the current axis
-   ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-
+   plt.rcParams.update({'font.size': 12})
+#  plt.rcParams.update({'line.linewidth': 8})
+#=========================
+   fig = plt.figure(figsize=(6,12))
+   ax1=fig.add_subplot(211)
+   ax1.scatter(gsi_observer_hofXBc,ufo, color='blue',label="rw", marker='o', s=3)
    plt.xlabel('gsi')
    plt.ylabel('ufo')
+   plt.title(thisobstype+':gsi and ufo hofx scatter')
+   figname='ufo_'+thisobstype+'_scatter.png'
 
-   #plt.xticks(np.arange(0,25,3))
+   ax2=fig.add_subplot(212)
+   ax2.scatter(gsi_observer_hofXBc,gsi_observer_hofX, color='blue',label="rw", marker='o', s=3)
+   plt.xlabel('gsi')
+   plt.ylabel('ufo')
+   plt.title(thisobstype+':gsihofXBc and hofX scatter')
 
-#ymin1=imos[0:8].min()
-#ymin2=gfs.min()
-#ymin=min(ymin1,ymin2)
-#ymin=ymin-1.0
-#print ymin
-
-#ymax1=imos[0:8].max()
-#ymax2=gfs.max()
-#ymax=max(ymax1,ymax2)
-#ymax=ymax+1.0
-
-#plt.ylim([ymin,ymax])
-   #plt.xlim([-25,25])
-
-   plt.savefig('ufo_rw_stage1.png',bbox_inches='tight',dpi=100)
-
-   fig1 = plt.figure(figsize=(8.0,7.5))
-   ax=fig1.add_subplot(111)
-   plt.hist(diff,bins=50)
-#  plt.xlim([-0.15,0.15])
-   plt.xlabel('gsi-ufo')
-   plt.savefig('ufo_rw_stage1_hist.png',bbox_inches='tight',dpi=100)
-
-   fig2 = plt.figure(figsize=(8.0,7.5))
-   ax=fig2.add_subplot(111)
-   plt.scatter(diff,geopotential_height, color='b',label="rw", marker='o', s=3)
-#  plt.xlim([-0.15,0.15])
-   plt.xlabel('gsi-ufo')
-   plt.ylabel('geop-height')
-   plt.savefig('ufo_rw_stage1_vdiff_scatter.png',bbox_inches='tight',dpi=100)
+   plt.savefig(figname,bbox_inches='tight',dpi=100)
+#=========================
 
 
-###--------------------------------------------------------------
-###--------------------------------------------------------------
+#=====================================================================
+#=====================================================================
 if __name__ == '__main__':
 
-   print("start ploting")
-
-   time=sys.argv[1]
-
-   #call subroutine plt_radar_ref
-   print("ploting obs radar reflectivity...")
-   titlename="obsref_"+time
-   fileame=sys.argv[1]
-   plt_ref_vertical(fileame)
+   ap = argparse.ArgumentParser(description='get JEDI-GDAS output')
+   ap.add_argument('-i','--inputfile', type=str, help='path to input YAML file for this analysis cycle', required=True)
+   ap.add_argument('-otype', '--obstype',   type=str, help='observation type', required=True)
+   ap.add_argument('-vname', '--varname',   type=str, help='variable name', required=True)
+   MyArgs=ap.parse_args()
+   
+   print("start ploting gsi hofx v.s. ufo hofx")
+   filename=MyArgs.inputfile
+   OBSTYPE=MyArgs.obstype
+   VarName=MyArgs.varname
+   plt_ufo_t(filename,OBSTYPE,VarName)
 
