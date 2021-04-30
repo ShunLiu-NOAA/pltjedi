c17e28e3b214b3cd52a29b75936ad7b33a683b9f
Shun Liu <sliu@Orion-login-4.HPC.MsState.Edu>
2021-04-30 09:10:21 -0500

    plt.sh

--- plt.sh
+++ plt.sh
@@ -1,2 +1,27 @@
-python plt_ufo_omb.py -i /work/noaa/da/sliu/R2D2/gfs/diag/a05q/PT6H/2020-12-16/gfs.a05q.diag.PT6H.sondes.2020-12-16T21:00:00Z.PT6H.nc4  \
-                    -otype T -vname air_temperature 
+#!/bin/bash
+
+genyaml=false
+
+if [ $genyaml = "true" ]; then
+   echo "generating confg.yaml"
+fi
+
+exp=a05v
+diagdir=/work/noaa/da/sliu/R2D2/gfs/diag/$exp/PT6H/tmp
+
+cat > config.yaml <<EOF
+paths:
+   inputdir: ${diagdir}
+   outputdir: ./
+VarName: air_temperature
+inputfile: diag
+OBSTYPE: T
+subtask: 5
+EOF
+
+python plt_ufo_omb.py
+
+
+
+#python plt_ufo_omb.py -i /work/noaa/da/sliu/R2D2/gfs/diag/a05v/PT6H/2020-12-14/gfs.a05v.diag.PT6H.sondes.2020-12-14T21:00:00Z.PT6H.nc4  \
+#                   -otype Ps -vname air_temperature
